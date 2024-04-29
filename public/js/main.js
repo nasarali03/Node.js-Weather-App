5de2f5c0166de2a61500576dded1e04e7c25f604
nasarali03 <nasarali1805@gmail.com>
2024-04-29 18:06:19 +0500

    Handling response from the backend

--- public/js/main.js
+++ public/js/main.js
@@ -16,7 +16,6 @@ const dataHide = document.querySelector(".middle_layer");
 const getInfo = async (event) => {
   event.preventDefault();
 
-  let apiKey = "";
   let city = cityName.value;
   if (city === "") {
     city_name.innerText = "Plz enter the name and then search";
@@ -45,51 +44,9 @@ const getInfo = async (event) => {
           dataHide.classList.remove("data_hide");
         });
     } catch (error) {
-      console.log("Error:", error);
+      city_name.innerText = "Plz enter the correct name and then search";
+      dataHide.classList.add("data_hide");
     }
-    // try {
-    //   const data = await response.json();
-    //   //   console.log(data);
-    //   const arrData = [data];
-
-    //   let currentDate = new Date();
-    //   let weekDay = new Array();
-    //   weekDay[0] = "Sunday";
-    //   weekDay[1] = "Monday";
-    //   weekDay[2] = "Tuesday";
-    //   weekDay[3] = "Wednesday";
-    //   weekDay[4] = "Thursday";
-    //   weekDay[5] = "Friday";
-    //   weekDay[6] = "Saturday";
-    //   let today = weekDay[currentDate.getDay()];
-    //   day.innerText = today;
-
-    //   var months = [
-    //     "Jan",
-    //     "Feb",
-    //     "Mar",
-    //     "Apr",
-    //     "May",
-    //     "Jun",
-    //     "Jul",
-    //     "Aug",
-    //     "Sep",
-    //     "Oct",
-    //     "Dec",
-    //   ];
-    //   let month = months[currentDate.getMonth()];
-    //   Month.innerText = month;
-    //   today_date.innerText = currentDate.getDate();
-    //   city_name.innerText = `${arrData[0].name}, ${arrData[0].sys.country}`;
-    //   temp.innerText = `${arrData[0].main.temp} `;
-
-    //   const icon = arrData[0].weather[0].icon;
-    //   imageIcon.innerHTML = `<img src="https://openweathermap.org/img/wn/${icon}@2x.png" width="100px"> `;
-    //   dataHide.classList.remove("data_hide");
-    // } catch (error) {
-    //   city_name.innerText = "Plz enter the correct name and then search";
-    //   dataHide.classList.add("data_hide");
-    // }
   }
 };
 
